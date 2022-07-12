a9295a71003aca171b3ff6b430ea1a295000dcf8
Suraj Chetry <s.chetry110@gmail.com>
2022-07-12 22:08:55 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import React, {useEffect, useState} from 'react'
 import {Button, FormControl, Input, InputLabel} from '@mui/material';
 import Todo from './Todo.js';
-import db from './firebase';
 // import {collection, onSnapshot, query, querysnapshot } from "firebase/firestore";
 // import { onSnapshot, collection, query, querySnapshot } from "firebase/firestore";
 
@@ -11,16 +10,6 @@ function App() {
   const [todos, setTodos]=useState([]);
   const [input, setInput]=useState('');
 
-  //when the app loads, we need to listen to the database and fetch new todos as they get added/removes 
-
-  useEffect(()=>{
-    // this code here ....fires when the app.js loads
-    db.collection('todos').onSnapshot( snapshot =>{
-    setTodos(snapshot.docs.map(doc => doc.data().todo))
-    })
-    }, []);   
-
- 
 
   const addTodo = (event)=>{
     event.preventDefault(); // stops refressing of forms
